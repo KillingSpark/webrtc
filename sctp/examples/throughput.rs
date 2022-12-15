b18467c74d40dd4a9c2a84b440303b24718e6c3c
Moritz Borcherding <moritz.borcherding@web.de>
2022-12-15 11:50:34 +0100

    resolving conflicts before commiting helps...

--- sctp/examples/throughput.rs
+++ sctp/examples/throughput.rs
@@ -136,11 +136,7 @@ fn main() -> Result<(), Error> {
 
                 let mut now = tokio::time::Instant::now();
                 let mut pkt_num = 0;
-<<<<<<< HEAD
-                while stream.write(&buf.clone().into()).is_ok() {
-=======
                 while stream.write(&buf.clone().into()).await.is_ok() {
->>>>>>> limit_pending_queue
                     pkt_num += 1;
                     if now.elapsed().as_secs() == 1 {
                         println!("Send {} pkts", pkt_num);
